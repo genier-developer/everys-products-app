c342e5acf5737203943dcac7dd9a6d6d1b68315a
Olga Genier <genier.developer@gmail.com>
2025-02-18 22:26:40 +0500

    feat: add styles for Search Page

--- src/pages/search/search.tsx
+++ src/pages/search/search.tsx
@@ -1,10 +1,11 @@
 import {FC} from "react";
 import {ProductSearch} from "../../features/search";
+import s from './search.module.scss'
 
 
 export const Search: FC = () => {
   return (
-    <div>
+    <div className={s.search}>
       <h1>Поиск</h1>
       <ProductSearch />
     </div>
