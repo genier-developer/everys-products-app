b35beba88ca7b2e0c52fc628c8c1295473f29ee7
Olga Genier <genier.developer@gmail.com>
2025-02-19 21:30:05 +0500

    fix: change Product List styles

--- src/features/search/ui/product-list/product-list.tsx
+++ src/features/search/ui/product-list/product-list.tsx
@@ -1,17 +1,22 @@
 import { useAppSelector } from '../../../../app/hooks.ts';
 import { StockItemModel } from '../../../../entities/product/types.ts';
-import {FC} from "react";
+import { FC } from "react";
 import s from './product-list.module.scss'
 
 export const ProductList: FC = () => {
-  const { items } = useAppSelector((state) => state.search);
-  if (!items) {
+  const { products, loading } = useAppSelector((state) => state.search);
+
+  if (loading) {
     return <div>Загрузка...</div>;
+  }
+
+  if (!products || !products.length) {
+    return <div>Товары не найдены</div>;
   }
 
   return (
     <div className={s.productList}>
-      {items.map((product: StockItemModel) => (
+      {products.map((product: StockItemModel) => (
         <div key={product.code || ''} className={s.productCard}>
           <h3>{product.title}</h3>
           <p>{product.description}</p>
